1bd8be115c5b52a8802b24b07b9f650830ea0783
root <root@DESKTOP-L1UKUDH.localdomain>
2022-10-10 04:06:20 -0700

    3

--- Django_projects/songcrud/musicapp/admin.py
+++ Django_projects/songcrud/musicapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Artiste, Song, Lyric
 
 # Register your models here.
+admin.site.register(Artiste, Song, Lyric)
--- Django_projects/songcrud/musicapp/models.py
+++ Django_projects/songcrud/musicapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    date_released = models.DateField(auto_now_add=True)
+    likes = models.IntegerFeild
+    artiste_id = models.IntegerField()
+
+class Lyric(models.Model):
+    content = models.TextField()
+    song_id = models.IntegerField()
